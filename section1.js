98a6f0ef4c4d66a54fbde70ee3a27ef83e5825d7
AJITHPULLANUR <ajiajith597@gmail.com>
2023-09-15 16:52:51 +0530

    projectedu

--- section1.js
+++ section1.js
@@ -2,9 +2,9 @@
 
 // Define an array of image URLs for the carousel
 const imageUrls = [
-    "20230914145944_[fpdl.in]_lake-with-houses-water_948735-174827_large.jpg",
-        "20230914142543_[fpdl.in]_cute-ducks-living-nature_23-2150702073_large.jpg",
-    "20230914143228_[fpdl.in]_from-golden-sands-rajasthan-emerald-backwaters-kerala-india-beckons-with-its-rich-tapestry-colors-flavors-cultures-illustration-generative-ai_850000-18512_medium.jpg"
+    "Section1Images/20230914145944_[fpdl.in]_lake-with-houses-water_948735-174827_large.jpg",
+    "Section1Images/20230914142543_[fpdl.in]_cute-ducks-living-nature_23-2150702073_large.jpg",
+    "Section1Images/20230914143228_[fpdl.in]_from-golden-sands-rajasthan-emerald-backwaters-kerala-india-beckons-with-its-rich-tapestry-colors-flavors-cultures-illustration-generative-ai_850000-18512_medium.jpg"
     // Add more image URLs here for additional slides
 ];
 
